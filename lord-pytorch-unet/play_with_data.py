637fa77064da767cc0c59aa89d488f85e72cddf1
Nirmaz <nir5591@gmail.com>
2021-09-05 23:38:45 +0300

    final

--- lord-pytorch-unet/play_with_data.py
+++
@@ -1,9 +0,0 @@
-import torch
-from os import listdir
-if __name__ == '__main__':
-    path = '/cs/casmip/nirm/embryo_project_version1/embryo_data_raw/before_preprocess/placenta'
-    a = listdir(path)
-    print(a)
-    path =  '/cs/casmip/nirm/embryo_project_version1/embryo_data_raw/before_preprocess/TRUFI'
-    b = listdir(path)
-    print(b)
